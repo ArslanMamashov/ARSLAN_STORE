18c0259c069100adf0f09a5234825f993d373822
MadeByArslan <arslanarslanov.12345@gmail.com>
2023-01-15 14:10:18 +0600

    O

---
+++ src/components/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Error = () => {
+  return (
+    <section className="section section-center text-center">
+      <h3>Error AHAHAHAHHAHAHAHHA! ;</h3>
+    </section>
+  );
+};
+
+export default Error;
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Private from "./PrivateRoute";
+import Checkout from "./CheckoutPage";
+
+export { Home, Products, SingleProduct, Cart, Error, About, Private, Checkout };
